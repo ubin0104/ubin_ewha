1adfa239f5f99e3e57d0aab77c45e1490316cc3a
ubin0104 <ybkk6216@gmail.com>
2019-12-17 03:52:30 +0900

    191217

---
+++ Documents/detect_mal/detect_mal/program/script.js
@@ -0,0 +1,41 @@
+//var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+//  Source: css-tricks
+//(function(){
+  //  var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+
+    //var link=document.location.href;
+    //document.write(newURL);
+//})
+function getCurrentTabUrl(callback){
+    var queryInfo={
+        active:true,
+        currentWindow:true
+    };
+
+    chrome.tabs.query(queryInfo,function(tabs){
+        var tab=tabs[0];
+        var url=tab.url;
+        callback(url);
+    });
+}
+function renderURL(statusText){
+    document.getElementById('urls').textContent=statusText;
+}
+//function send(url){
+//   document.location.href="localhost/api/whites/"+url;
+//}
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    var link=document.getElementById('getUrl');
+
+    link.addEventListener('click',function(){
+        getCurrentTabUrl(function(url){
+            renderURL(url);
+            var link="localhost:8000/check/"+url;
+            window.open(link,'_blank');
+        });
+    });
+});
+
+
